4c02417678dfa70c8f2c1af45864759552c1da0d
Wouter He <2236629703@qq.com>
2022-02-12 21:00:46 +0800

    fix

--- src/views/Home/service.vue
+++ src/views/Home/service.vue
@@ -65,12 +65,12 @@
 <script>
 import {checkPhoneValidator} from 'utils/validator';
 const tableItem = {
-  date: "2019-05-20", // 日期
-  name: "被删", // 姓名
-  phone: "13888888888", // 电话
-  address: "深圳市南山区滨海大道 888 号" // 地址
+  date: "2022-01-01", // 日期
+  name: "李华", // 姓名
+  phone: "1234567890", // 电话
+  address: "深圳市南山区滨海大道 64 号" // 地址
 };
-// 此处先以 tableItem 为数据源，拷贝生成 20 个数据，然后再根据索引 index 添加上 id
+
 const tableData = Array(20).fill(tableItem).map((x, i) => {return {id: i + 1, ...x};});
 
 // 下面是 Vue 组件
